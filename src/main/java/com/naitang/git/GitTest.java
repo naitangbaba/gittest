fbaf2fd66733ffb47914b3a6bdb291a0ec5456c8
naitang <1515319126@qq.com>
2023-03-01 09:32:29 +0800

    hot-fix first commit

--- src/main/java/com/naitang/git/GitTest.java
+++ src/main/java/com/naitang/git/GitTest.java
@@ -9,6 +9,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hai,git");
         System.out.println("hai git");
+        System.out.println("ha,git");
     }
 
 }
