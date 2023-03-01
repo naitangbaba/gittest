05c34b7b7e71e4ac225305bbbeaa06bfd37aadee
naitang <1515319126@qq.com>
2023-03-01 09:18:09 +0800

    first commit

---
+++ src/main/java/com/naitang/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.naitang.git;
+
+/**
+ * @author naitang
+ * @create 2023-03-01  9:11
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hai,git");
+    }
+
+}
